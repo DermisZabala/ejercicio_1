e9835408d549accedeca4e00e585a99f0d1612c9
DermisZabala <dermiszabala@gmail.com>
2024-03-03 18:12:05 -0400

    Calculadora de porcentaje de propina

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Descripción: Una aplicación de consola que calcula la propina basada en el monto total de la factura
+            //y el porcentaje de propina ingresado por el usuario.
+
+            Console.WriteLine("Calcular el porcentaje de la propina");
+
+            Console.WriteLine("Ingrese el monto total");
+            double montoA = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("El la cantidad de propina");
+            double totalPropina = double.Parse(Console.ReadLine());
+
+            double porcentajePropina = (totalPropina / montoA) * 100;
+
+            Console.WriteLine($"El porcentaje de la propina fue de {porcentajePropina}%");
+
+            Console.ReadKey();
+
+        }
+    }
+}
